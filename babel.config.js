2004034d6aafb4cc4c5e7cd8966ad7345a8e5bf4
Karol Blaut <blaut.karol@gmail.com>
2023-06-11 23:00:38 +0200

    Praca nad searchbarem

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+	rts = {
+		  presets: ['@babel/preset-env', '@babel/preset-react'],
+	};
+
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+};
--- src/data/icons.js
+++ src/data/icons.js
@@ -1,10 +1,9 @@
-import { faBed, faCalendarDays, faCar, faPerson, faPlane, faTaxi } from "@fortawesome/free-solid-svg-icons";
+import { faBed, faCalendarDays, faCar, faPerson, faPlane } from "@fortawesome/free-solid-svg-icons";
 
 export const icons = [
-  faBed,
-  faCalendarDays,
-  faCar,
-  faPerson,
-  faPlane,
-  faTaxi,
+  { icon: faBed, name: "Stays", spanText: "Where are you going?" },
+  { icon: faCalendarDays, name: "Flights", spanText: "date to date" },
+  { icon: faCar, name: "Car rentals", spanText: "Pick-up and drop-off" },
+  { icon: faPerson, name: "Attractions", spanText: "2 adults person" },
+  { icon: faPlane, name: "Airport taxis", spanText: "Airport transfers" },
 ];
